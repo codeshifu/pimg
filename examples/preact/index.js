866b2281bdfeae3c7b1bd8a585a74b2cd25db8d6
Ademola Adegbuyi <ooade96@gmail.com>
2018-01-27 22:34:20 +0100

    Add scroll to Review and thumbnail opt

--- examples/preact/index.js
+++ examples/preact/index.js
@@ -6,16 +6,162 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1> Hello, World! </h1>{' '}
+				<h1> Hello, World! </h1>
+				<div>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+					nec metus ut ante efficitur lacinia elementum quis mauris. Vivamus
+					hendrerit sollicitudin urna, non cursus ex vulputate a. Etiam non
+					ligula odio. Fusce a pellentesque nisl. Ut dapibus a ligula ac
+					rhoncus. Duis dignissim sit amet lorem quis molestie. Quisque eu
+					lacinia lacus, quis pulvinar erat. Fusce consequat magna eget libero
+					faucibus pretium. Nullam tempor tortor nec velit mollis mattis. Sed
+					eget tincidunt justo, eu dapibus ipsum. Maecenas lorem libero, rhoncus
+					id enim eu, dapibus cursus ante. Sed nec congue massa. Maecenas ut
+					elit nec massa sollicitudin bibendum. Nam viverra gravida sem,
+					pharetra tincidunt sapien fermentum nec. Donec dolor massa, rhoncus
+					non dolor in, laoreet vestibulum neque. Donec hendrerit aliquam felis,
+					ac faucibus nibh tempus non. Nam aliquet, nisi at vulputate cursus,
+					eros arcu pellentesque leo, ac tempor odio elit eu urna. Nullam
+					consequat libero quis euismod condimentum. Etiam cursus lectus vitae
+					tempus ultrices. Integer porta erat lacus, in blandit massa porttitor
+					in. Quisque sit amet augue augue. Quisque ut scelerisque ligula.
+					Praesent eros lectus, pulvinar nec enim eget, mollis euismod elit.
+					Fusce tincidunt orci ut nibh lobortis, at cursus erat pharetra. In
+					tristique commodo feugiat. Morbi eu mi nisl. Etiam luctus tincidunt
+					sagittis. Integer orci purus, pulvinar vel erat a, consectetur
+					interdum tortor. Aliquam suscipit vestibulum elementum. Quisque mi
+					felis, porta id ultricies at, cursus id neque. Integer scelerisque
+					dolor diam, non porta ante lobortis eget. Fusce porta tempor odio, et
+					elementum lacus. Praesent lacinia viverra sapien, a pharetra tellus
+					fermentum non. Phasellus vehicula nibh ultrices metus sagittis dictum.
+					Etiam lobortis lectus massa, vel tincidunt tortor placerat ut. Ut eget
+					malesuada nisi. Nullam aliquet semper ultricies. Aliquam sit amet eros
+					ac justo blandit hendrerit. In pellentesque fringilla ultricies.
+					Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci
+					luctus et ultrices posuere cubilia Curae; Proin pharetra metus a augue
+					ultricies convallis. Sed sagittis laoreet maximus. Curabitur lectus
+					felis, molestie id nunc id, fermentum congue ipsum. Suspendisse
+					potenti. Donec eget convallis libero, vel hendrerit leo. Suspendisse
+					eleifend magna eget volutpat imperdiet. Pellentesque et maximus magna.
+					Ut ultrices convallis mauris, quis tincidunt libero mollis id.
+					Phasellus et maximus odio, nec ultrices augue. Nam ante tellus,
+					commodo ac hendrerit ut, laoreet a eros. Nulla nisl erat, dapibus eu
+					egestas ut, sodales vel tellus. Etiam tempor, ligula sed efficitur
+					euismod, orci orci dictum ligula, vel efficitur ligula risus eu nibh.
+					Pellentesque quis accumsan libero, id egestas elit. Etiam mollis,
+					metus a semper accumsan, enim turpis lobortis mi, eu mattis tellus
+					odio non nunc. Praesent vel nulla fermentum nunc egestas iaculis eget
+					in risus. Phasellus ac odio sodales, porta purus a, imperdiet felis.
+					Vivamus arcu justo, mollis vel venenatis porttitor, rutrum eu leo.
+					Proin egestas gravida ipsum sed pulvinar. Curabitur feugiat massa sed
+					faucibus euismod. Cras quis ipsum convallis, sagittis nulla consequat,
+					rhoncus enim. Praesent ac ultricies enim, eget mattis velit. Nulla
+					lorem mauris, varius sed sollicitudin vel, molestie non neque. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+					metus ut ante efficitur lacinia elementum quis mauris. Vivamus
+					hendrerit sollicitudin urna, non cursus ex vulputate a. Etiam non
+					ligula odio. Fusce a pellentesque nisl. Ut dapibus a ligula ac
+					rhoncus. Duis dignissim sit amet lorem quis molestie. Quisque eu
+					lacinia lacus, quis pulvinar erat. Fusce consequat magna eget libero
+					faucibus pretium. Nullam tempor tortor nec velit mollis mattis. Sed
+					eget tincidunt justo, eu dapibus ipsum. Maecenas lorem libero, rhoncus
+					id enim eu, dapibus cursus ante. Sed nec congue massa. Maecenas ut
+					elit nec massa sollicitudin bibendum. Nam viverra gravida sem,
+					pharetra tincidunt sapien fermentum nec. Donec dolor massa, rhoncus
+					non dolor in, laoreet vestibulum neque. Donec hendrerit aliquam felis,
+					ac faucibus nibh tempus non. Nam aliquet, nisi at vulputate cursus,
+					eros arcu pellentesque leo, ac tempor odio elit eu urna. Nullam
+					consequat libero quis euismod condimentum. Etiam cursus lectus vitae
+					tempus ultrices. Integer porta erat lacus, in blandit massa porttitor
+					in. Quisque sit amet augue augue. Quisque ut scelerisque ligula.
+					Praesent eros lectus, pulvinar nec enim eget, mollis euismod elit.
+					Fusce tincidunt orci ut nibh lobortis, at cursus erat pharetra. In
+					tristique commodo feugiat. Morbi eu mi nisl. Etiam luctus tincidunt
+					sagittis. Integer orci purus, pulvinar vel erat a, consectetur
+					interdum tortor. Aliquam suscipit vestibulum elementum. Quisque mi
+					felis, porta id ultricies at, cursus id neque. Integer scelerisque
+					dolor diam, non porta ante lobortis eget. Fusce porta tempor odio, et
+					elementum lacus. Praesent lacinia viverra sapien, a pharetra tellus
+					fermentum non. Phasellus vehicula nibh ultrices metus sagittis dictum.
+					Etiam lobortis lectus massa, vel tincidunt tortor placerat ut. Ut eget
+					malesuada nisi. Nullam aliquet semper ultricies. Aliquam sit amet eros
+					ac justo blandit hendrerit. In pellentesque fringilla ultricies.
+					Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci
+					luctus et ultrices posuere cubilia Curae; Proin pharetra metus a augue
+					ultricies convallis. Sed sagittis laoreet maximus. Curabitur lectus
+					felis, molestie id nunc id, fermentum congue ipsum. Suspendisse
+					potenti. Donec eget convallis libero, vel hendrerit leo. Suspendisse
+					eleifend magna eget volutpat imperdiet. Pellentesque et maximus magna.
+					Ut ultrices convallis mauris, quis tincidunt libero mollis id.
+					Phasellus et maximus odio, nec ultrices augue. Nam ante tellus,
+					commodo ac hendrerit ut, laoreet a eros. Nulla nisl erat, dapibus eu
+					egestas ut, sodales vel tellus. Etiam tempor, ligula sed efficitur
+					euismod, orci orci dictum ligula, vel efficitur ligula risus eu nibh.
+					Pellentesque quis accumsan libero, id egestas elit. Etiam mollis,
+					metus a semper accumsan, enim turpis lobortis mi, eu mattis tellus
+					odio non nunc. Praesent vel nulla fermentum nunc egestas iaculis eget
+					in risus. Phasellus ac odio sodales, porta purus a, imperdiet felis.
+					Vivamus arcu justo, mollis vel venenatis porttitor, rutrum eu leo.
+					Proin egestas gravida ipsum sed pulvinar. Curabitur feugiat massa sed
+					faucibus euismod. Cras quis ipsum convallis, sagittis nulla consequat,
+					rhoncus enim. Praesent ac ultricies enim, eget mattis velit. Nulla
+					lorem mauris, varius sed sollicitudin vel, molestie non neque. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+					metus ut ante efficitur lacinia elementum quis mauris. Vivamus
+					hendrerit sollicitudin urna, non cursus ex vulputate a. Etiam non
+					ligula odio. Fusce a pellentesque nisl. Ut dapibus a ligula ac
+					rhoncus. Duis dignissim sit amet lorem quis molestie. Quisque eu
+					lacinia lacus, quis pulvinar erat. Fusce consequat magna eget libero
+					faucibus pretium. Nullam tempor tortor nec velit mollis mattis. Sed
+					eget tincidunt justo, eu dapibus ipsum. Maecenas lorem libero, rhoncus
+					id enim eu, dapibus cursus ante. Sed nec congue massa. Maecenas ut
+					elit nec massa sollicitudin bibendum. Nam viverra gravida sem,
+					pharetra tincidunt sapien fermentum nec. Donec dolor massa, rhoncus
+					non dolor in, laoreet vestibulum neque. Donec hendrerit aliquam felis,
+					ac faucibus nibh tempus non. Nam aliquet, nisi at vulputate cursus,
+					eros arcu pellentesque leo, ac tempor odio elit eu urna. Nullam
+					consequat libero quis euismod condimentum. Etiam cursus lectus vitae
+					tempus ultrices. Integer porta erat lacus, in blandit massa porttitor
+					in. Quisque sit amet augue augue. Quisque ut scelerisque ligula.
+					Praesent eros lectus, pulvinar nec enim eget, mollis euismod elit.
+					Fusce tincidunt orci ut nibh lobortis, at cursus erat pharetra. In
+					tristique commodo feugiat. Morbi eu mi nisl. Etiam luctus tincidunt
+					sagittis. Integer orci purus, pulvinar vel erat a, consectetur
+					interdum tortor. Aliquam suscipit vestibulum elementum. Quisque mi
+					felis, porta id ultricies at, cursus id neque. Integer scelerisque
+					dolor diam, non porta ante lobortis eget. Fusce porta tempor odio, et
+					elementum lacus. Praesent lacinia viverra sapien, a pharetra tellus
+					fermentum non. Phasellus vehicula nibh ultrices metus sagittis dictum.
+					Etiam lobortis lectus massa, vel tincidunt tortor placerat ut. Ut eget
+					malesuada nisi. Nullam aliquet semper ultricies. Aliquam sit amet eros
+					ac justo blandit hendrerit. In pellentesque fringilla ultricies.
+					Suspendisse potenti. Vestibulum ante ipsum primis in faucibus orci
+					luctus et ultrices posuere cubilia Curae; Proin pharetra metus a augue
+					ultricies convallis. Sed sagittis laoreet maximus. Curabitur lectus
+					felis, molestie id nunc id, fermentum congue ipsum. Suspendisse
+					potenti. Donec eget convallis libero, vel hendrerit leo. Suspendisse
+					eleifend magna eget volutpat imperdiet. Pellentesque et maximus magna.
+					Ut ultrices convallis mauris, quis tincidunt libero mollis id.
+					Phasellus et maximus odio, nec ultrices augue. Nam ante tellus,
+					commodo ac hendrerit ut, laoreet a eros. Nulla nisl erat, dapibus eu
+					egestas ut, sodales vel tellus. Etiam tempor, ligula sed efficitur
+					euismod, orci orci dictum ligula, vel efficitur ligula risus eu nibh.
+					Pellentesque quis accumsan libero, id egestas elit. Etiam mollis,
+					metus a semper accumsan, enim turpis lobortis mi, eu mattis tellus
+					odio non nunc. Praesent vel nulla fermentum nunc egestas iaculis eget
+					in risus. Phasellus ac odio sodales, porta purus a, imperdiet felis.
+					Vivamus arcu justo, mollis vel venenatis porttitor, rutrum eu leo.
+					Proin egestas gravida ipsum sed pulvinar. Curabitur feugiat massa sed
+					faucibus euismod. Cras quis ipsum convallis, sagittis nulla consequat,
+					rhoncus enim. Praesent ac ultricies enim, eget mattis velit. Nulla
+					lorem mauris, varius sed sollicitudin vel, molestie non neque.
+				</div>
 				<Image
+					scrollToReveal
 					className="myImage"
+					thumbnail="https://res.cloudinary.com/stackpie/image/upload/c_thumb,w_10/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
 					src="https://res.cloudinary.com/stackpie/image/upload/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
-				>
-					<Thumbnail
-						className="myImage__thumb"
-						src="https://res.cloudinary.com/stackpie/image/upload/c_thumb,w_10/v1514275012/assassins_creed_revelations_concept_art-wallpaper-1366x768_qdl9vo.jpg"
-					/>
-				</Image>{' '}
+				/>
 			</div>
 		)
 	}
